39dd2f4b40f54768983f5800c4a51ef9f9c9e077
gerard ketefian <gerard.ketefian@noaa.gov>
2020-11-05 02:25:36 -0700

    Update date-dependent file naming convention for FV3GFS external model files on HPSS.

--- ush/get_extrn_mdl_file_dir_info.sh
+++ ush/get_extrn_mdl_file_dir_info.sh
@@ -784,12 +784,17 @@ has not been specified for this external model:
   "FV3GFS")
     if [ "${fv3gfs_file_fmt}" = "nemsio" ]; then
  
-      if [ "${cdate_FV3LAM}" -le "2019061206" ]; then
+#      if [ "${cdate_FV3LAM}" -le "2019061206" ]; then
+      if [ "${cdate_FV3LAM}" -lt "2019061200" ]; then
         arcv_dir="/NCEPDEV/emc-global/5year/emc.glopara/WCOSS_C/Q2FY19/prfv3rt3/${cdate_FV3LAM}"
         arcv_fns=""
-      else
+      elif [ "${cdate_FV3LAM}" -ge "2019061200" ] && \
+           [ "${cdate_FV3LAM}" -lt "2020022600" ]; then
         arcv_dir="/NCEPPROD/hpssprod/runhistory/rh${yyyy}/${yyyy}${mm}/${yyyymmdd}"
         arcv_fns="gpfs_dell1_nco_ops_com_gfs_prod_gfs.${yyyymmdd}_${hh}."
+      elif [ "${cdate_FV3LAM}" -ge "2020022600" ]; then
+        arcv_dir="/NCEPPROD/hpssprod/runhistory/rh${yyyy}/${yyyy}${mm}/${yyyymmdd}"
+        arcv_fns="com_gfs_prod_gfs.${yyyymmdd}_${hh}."
       fi
       arcv_fmt="tar"
       if [ "${anl_or_fcst}" = "ANL" ]; then
